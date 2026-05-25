37cc83130ddeb40da5f76792a6059094d4de751a
Mohammed Farhan k <farhancherushola@gmail.com>
2026-05-25 20:24:13 +0530

    Time: 9 ms (63.02%) | Memory: 12.4 MB (18.23%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0:
-            return False
-
         num = x
         rev = 0
 
-        while num != 0:
+        while num > 0:
             rev = rev * 10 + num % 10
             num = num // 10
-
-        return rev == x       
+        return rev == x          

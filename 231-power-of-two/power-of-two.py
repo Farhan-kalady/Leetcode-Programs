fd8cd51bb52627f6a593839dc30edbcc84c2082b
Mohammed Farhan k <farhancherushola@gmail.com>
2026-07-07 11:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (88.77%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        elif n & n -1 == 0 :
+            return True
+        else:
+            return False    

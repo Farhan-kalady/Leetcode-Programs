297ce8756a067442bd2a71b8c7a220a2a2735824
Mohammed Farhan k <farhancherushola@gmail.com>
2026-07-24 05:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (70.36%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,9 +1,13 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         n = len(nums)
         low = 0
         high = n - 1
-        
         while low <= high:
             mid = (low + high) // 2
             if nums[mid] == target:
@@ -11,5 +15,6 @@ class Solution:
             elif nums[mid] > target:
                 high = mid - 1
             else:
-                low = mid + 1       
-        return -1         
+                low = mid + 1
+        return -1                
+        

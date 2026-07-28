fd37e1c6acb75a194f4aca5adb823c752b61f1f3
Mohammed Farhan k <farhancherushola@gmail.com>
2026-07-28 07:36:36 +0530

    Time: 19 ms (16.49%) | Memory: 20.2 MB (18.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,21 +1,9 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        char_freq = {}
-        for ch in s:
-            char_freq[ch] = char_freq.get(ch,0) + 1
-        for ch in t:
-            if ch not in char_freq:
-                return False
-            else:
-                if char_freq[ch] == 0:
-                    return False
-                else:
-                    char_freq[ch] -= 1
-        return True                            
+        sort_s = sorted(s)
+        sort_t = sorted(t)
+        if sort_s == sort_t:
+            return True
+        return False       

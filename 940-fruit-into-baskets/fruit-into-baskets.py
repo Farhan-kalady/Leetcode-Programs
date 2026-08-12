1249b12bb04ea54a1a5bff1d290d9dfa9ed6202c
Mohammed Farhan k <farhancherushola@gmail.com>
2026-08-12 22:18:03 +0530

    Time: 307 ms (8.36%) | Memory: 17.1 MB (28.92%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = 0
+        maxi = 0
+        n = len(fruits)
+        my_dict = {}
+        while right < n:
+            my_dict[fruits[right]] = my_dict.get(fruits[right], 0) + 1
+            while len(my_dict) > 2:
+                my_dict[fruits[left]] -= 1
+                if my_dict[fruits[left]] == 0:
+                    del my_dict[fruits[left]]
+                left += 1
+            if len(my_dict) <= 2:
+                maxi = max(maxi, right - left + 1) 
+            right += 1
+        return maxi       

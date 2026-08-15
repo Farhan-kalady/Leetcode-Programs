4bbd272359d2ce1a5073da396f7ecd792885f86f
Mohammed Farhan k <farhancherushola@gmail.com>
2026-08-15 21:23:00 +0530

    Time: 3 ms (86.29%) | Memory: 13.2 MB (99.59%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        nums = sorted(nums) 
+        return nums[n/2]
+        
+

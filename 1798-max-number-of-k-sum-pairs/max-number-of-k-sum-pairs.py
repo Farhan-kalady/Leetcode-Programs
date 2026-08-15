733f94d3c48a800d039c42fcd79f7cd3b8b20a7f
Mohammed Farhan k <farhancherushola@gmail.com>
2026-08-15 23:21:17 +0530

    Time: 539 ms (82.54%) | Memory: 21.6 MB (78.20%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,14 +1,26 @@
-class Solution:
-    def maxOperations(self, nums: List[int], k: int) -> int:
-        hashmap = {}
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
         count = 0
-        for num in nums:
-            complement = k - num
-            if hashmap.get(complement, 0) > 0:
+        nums.sort()
+        n = len(nums)
+        left = 0
+        right = n - 1
+        while left < right:
+            if nums[left] + nums[right] == k:
                 count += 1
-                hashmap[complement] -= 1
-            else:    
-          
-                hashmap[num] = hashmap.get(num, 0) + 1
-        return count        
-        
+                left += 1
+                right -= 1
+            elif nums[left] + nums[right] < k :
+                left += 1
+            else:
+                right -= 1
+        return count                
+
+
+                       
+                  

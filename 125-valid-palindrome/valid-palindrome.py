bc7670dacfc1cdb41493a683f98a8f798504b5ec
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-02 19:19:05 +0530

    Time: 239 ms (14.82%) | Memory: 13.1 MB (36.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,13 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         clean = ""
         for ch in s:
             if ch.isalnum():
                 clean += ch.lower()
         return clean == clean[::-1]        
-
+                
         

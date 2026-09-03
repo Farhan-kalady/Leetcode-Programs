955954cc94aa5514ddcc02252fb0bc1d0a403c70
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-03 10:15:00 +0530

    Time: 21 ms (52.44%) | Memory: 12.5 MB (86.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        sort_s = sorted(s)
-        sort_t = sorted(t)
-        return sort_s == sort_t                            
+        if len(s) != len(t):
+            return False
+        char_freq = {}
+        for ch in s:
+            char_freq[ch] = char_freq.get(ch,0) + 1
+        for ch in t:
+            if ch not in char_freq:
+                return False
+            else:
+                if char_freq[ch] == 0:
+                    return False
+                else:
+                    char_freq[ch] -= 1
+        return True

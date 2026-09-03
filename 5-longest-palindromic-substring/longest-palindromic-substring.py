ef18d1dece07f15f264968589eaab5806e63f2d4
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-03 07:19:20 +0530

    Time: 410 ms (34.58%) | Memory: 12.4 MB (54.28%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,26 +4,34 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        if not s:
-            return ""
-
-        def expand_around_center(s,left,right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-               left -= 1
-               right += 1
-            return right - left - 1   
-
+        if len(s) < 2:
+            return s
         start = 0
-        end = 0
-
-        for i in range(len(s)):
-            odd = expand_around_center(s, i, i)
-            even = expand_around_center(s, i, i+1)
-            max_len = max(odd, even)
-
-            if max_len > end - start:
-                start = i - (max_len -1) // 2
-                end = i + max_len // 2
-        return s[start:end+1]                    
-
+        max_len = 1
         
+        for i in range(len(s)):
+            left = i
+            right = i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max_len:
+                    start = left
+                    max_len = right - left + 1
+
+                left -= 1
+                right += 1
+
+            left = i
+            right = i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if right - left + 1 > max_len:
+                    start = left
+                    max_len = right - left + 1
+
+                left -= 1
+                right += 1
+
+        return s[start:start+max_len]            
+
+
+                
+                    

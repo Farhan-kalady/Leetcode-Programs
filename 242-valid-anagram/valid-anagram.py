8880a267842017827f3e9c3bed1271925e2f7816
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-03 10:09:08 +0530

    Time: 29 ms (7.62%) | Memory: 13.4 MB (39.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,10 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-       hash_s = {}
-       hash_t = {}
-       for ch in s:
-        hash_s[ch] = hash_s.get(ch, 0) + 1
-       for ch in t:
-        hash_t[ch] = hash_t.get(ch, 0) + 1
-       return hash_s == hash_t  
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        sort_s = sorted(s)
+        sort_t = sorted(t)
+        return sort_s == sort_t                            

9fb0af04c8987bd266a052b348236d6b93885539
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-14 06:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (94.27%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,6 +1,13 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
         n = len(nums)
-        sum_num = sum(nums)
-        miss = (n*(n + 1)) // 2 - sum_num
-        return miss
+        cur_sum = sum(nums)
+        expec_sum = (n*(n + 1))//2
+        missing = expec_sum - cur_sum
+        return missing
+        

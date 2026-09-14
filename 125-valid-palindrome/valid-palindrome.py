55fca5397d56561a0349ccdecff0464672f39258
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-14 06:51:12 +0530

    Time: 15 ms (71.60%) | Memory: 12.7 MB (76.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,10 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        clean = ""
-        for ch in s:
-            if ch.isalnum():
-                clean += ch.lower()
-        return clean == clean[::-1]               
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return  False
+            left += 1
+            right -= 1
+        return True                           
                 
         

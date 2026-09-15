276dc8eceb6b266c6e2af1038447203a7050b254
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-15 06:55:32 +0530

    Time: 11 ms (82.58%) | Memory: 22.1 MB (53.48%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         group = {}
         for word in strs:
-            key = ''.join(sorted(word))
+            key = " ".join(sorted(word))
             if key not in group:
                 group[key] = []
-            group[key].append(word)
+            group[key].append(word) 
 
-        return list(group.values())        
+        return list(group.values())       

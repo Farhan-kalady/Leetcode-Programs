1dfa12334534bacb17b144196436b01be95f1baa
Mohammed Farhan k <farhancherushola@gmail.com>
2026-09-16 09:47:35 +0530

    Time: 237 ms (25.21%) | Memory: 20 MB (29.59%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,12 +3,15 @@ class Solution:
         left = 0
         right = 0
         my_dict = {}
-        maxi = 0
+        
+        total = 0
         while right < len(s):
             if s[right] in my_dict:
                 left = max(left, my_dict[s[right]] + 1)
-            maxi = max(maxi, right - left + 1)
+            res = right - left + 1   
+            total = max(total, res) 
             my_dict[s[right]] = right
             right += 1
-        return maxi        
+        return total    
+
 
